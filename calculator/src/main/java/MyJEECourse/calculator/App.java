53579d4cae5e39153c2d67bd54b14a1d284038f5
unknown <michel.ruizf@gmail.com>
2011-05-26 19:53:21 -0300

    Initial Project Structure

---
+++ calculator/src/main/java/MyJEECourse/calculator/App.java
@@ -0,0 +1,13 @@
+package MyJEECourse.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
